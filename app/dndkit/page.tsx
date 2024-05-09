d89a385576088d9b9b4c9adc5418ce65651b4f22
oak072544 <oak072544@gmail.com>
2024-05-09 19:43:48 +0700

    exaple page change to shortable dnd

--- app/dndkit/page.tsx
+++ app/dndkit/page.tsx
@@ -1,102 +1,31 @@
 "use client"
-import React, { useState } from 'react';
-import Head from 'next/head';
+import React from 'react';
+import { MeasuringStrategy } from '@dnd-kit/core';
 import {
-    closestCenter,
-    closestCorners,
-    rectIntersection,
-    pointerWithin,
-    DndContext,
-    useDraggable,
-    UniqueIdentifier,
-    CollisionDetection as CollisionDetectionType,
-    Modifiers,
-} from '@dnd-kit/core';
+    arraySwap,
+    AnimateLayoutChanges,
+    defaultAnimateLayoutChanges,
+    rectSortingStrategy,
+    rectSwappingStrategy,
+} from '@dnd-kit/sortable';
 
-import {
-    Draggable,
-    DraggableOverlay,
-    Droppable,
-    GridContainer,
-    Wrapper,
-} from '../../components';
+import { Sortable, Props as SortableProps } from './Sortable';
+import { GridContainer } from '../../components';
 
-interface Props {
-    collisionDetection?: CollisionDetectionType;
-    containers?: string[];
-    modifiers?: Modifiers;
-    value?: string;
-}
-
-function DroppableStory({
-    containers = ['A'],
-    collisionDetection,
-    modifiers,
-}: Props) {
-    const [isDragging, setIsDragging] = useState(false);
-    const [parent, setParent] = useState<UniqueIdentifier | null>(null);
-
-    const item = <DraggableItem />;
-
-    return (
-        <>
-            <Head>
-                <title>Core/Droppable/useDroppable</title>
-            </Head>
-            <DndContext
-                collisionDetection={collisionDetection}
-                modifiers={parent === null ? undefined : modifiers}
-                onDragStart={() => setIsDragging(true)}
-                onDragEnd={({ over }) => {
-                    setParent(over ? over.id : null);
-                    setIsDragging(false);
-                }}
-                onDragCancel={() => setIsDragging(false)}
-            >
-                <Wrapper>
-                    <Wrapper style={{ width: 350, flexShrink: 0 }}>
-                        {parent === null ? item : null}
-                    </Wrapper>
-                    <GridContainer columns={2}>
-                        {containers.map((id) => (
-                            <Droppable key={id} id={id} dragging={isDragging}>
-                                {parent === id ? item : null}
-                            </Droppable>
-                        ))}
-                    </GridContainer>
-                </Wrapper>
-                <DraggableOverlay />
-            </DndContext>
-        </>
-    );
-}
-
-interface DraggableProps {
-    handle?: boolean;
-}
-
-function DraggableItem({ handle }: DraggableProps) {
-    const { isDragging, setNodeRef, listeners } = useDraggable({
-        id: 'draggable-item',
-    });
-
-    return (
-        <Draggable
-            dragging={isDragging}
-            ref={setNodeRef}
-            handle={handle}
-            listeners={listeners}
-            style={{
-                opacity: isDragging ? 0 : undefined,
-            }}
-        />
-    );
-}
-
-function MultipleDroppables() {
-    return (
-        <DroppableStory containers={['A', 'B', 'C', 'E']} />
-    )
+const props: Partial<SortableProps> = {
+    adjustScale: true,
+    Container: (props: any) => <GridContainer {...props} columns={5} />,
+    strategy: rectSortingStrategy,
+    wrapperStyle: () => ({
+        width: 140,
+        height: 140,
+    }),
 };
 
-export default MultipleDroppables;
+export default function Grid() {
+    return (
+
+        <Sortable {...props} />
+
+    )
+}
